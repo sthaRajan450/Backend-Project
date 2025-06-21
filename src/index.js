f5452a89dd36d5a09158910a47c42173aa1cc8a7
sthaRaj9841 <thaneshworshrestha499@gmail.com>
2025-06-21 17:17:12 +0545

    utils code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:"./.env"
-})
-connectDB();
+  path: "./.env",
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Sever is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed!!", err);
+  });
